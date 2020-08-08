8655352b40891b14099eeeefa2d0c81d2d87a7b7
Jeff Toppings <jeffrey.toppings@outlook.com>
2020-08-08 13:26:43 -0600

    finish up About me section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { PhotoJobTitleComponent } from './photo-job-title/photo-job-title.component';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
+import {PhotoJobTitleComponent} from './photo-job-title/photo-job-title.component';
 import {MatButtonModule} from '@angular/material/button';
-import { SectionDividerComponent } from './section-divider/section-divider.component';
-import { ProjectsComponent } from './projects/projects.component';
+import {SectionDividerComponent} from './section-divider/section-divider.component';
+import {ProjectsComponent} from './projects/projects.component';
 import {MatCardModule} from '@angular/material/card';
-import { AboutMeComponent } from './about-me/about-me.component';
+import {AboutMeComponent} from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
     PhotoJobTitleComponent,
     SectionDividerComponent,
     ProjectsComponent,
-    AboutMeComponent
+    AboutMeComponent,
   ],
   imports: [
     BrowserModule,
